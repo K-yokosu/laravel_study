f3428d747d56788cfe0a41a54284598145ea1200
横田昴輝 <kokiyokotakoki@gmail.com>
2023-08-14 20:00:46 +0900

    name付けたらprofile見つからん時にloginに遷移する。なんで？

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/register',[\App\Http\Controllers\UserController::class,'showRegister']);
 Route::post('/register',[\App\Http\Controllers\UserController::class,'register']);
 
-Route::get('/login',[\App\Http\Controllers\UserController::class,'showLogin']);
+Route::get('/login',[\App\Http\Controllers\UserController::class,'showLogin'])->name('login');
 Route::post('/login',[\App\Http\Controllers\UserController::class,'login']);
 
 Route::middleware('auth')->group(function (){
